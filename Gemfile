9e3b5492fc6eedf50b9fd8b54ce4d730fd0f7595
Andrew Vos <andrew.vos@gmail.com>
2017-03-20 19:24:13 +0000

    Add devise

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'devise'
+
 gem 'google-cloud'
 
 gem 'dotenv-rails'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_CLASSES = {
+    notice: 'alert alert-info',
+    success: 'alert alert-success',
+    error: 'alert alert-danger',
+    alert: 'alert alert-warning'
+  }.with_indifferent_access.freeze
+
+  def flash_class(level)
+    BOOTSTRAP_FLASH_CLASSES.fetch(level)
+  end
 end
